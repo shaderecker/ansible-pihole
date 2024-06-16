7c4500b6bd2193db3f2d26990c6bd5662ecd89fb
Campbell Vertesi <campbell@vertesi.com>
2024-06-17 00:27:06 +0200

    Add hardcoded dhcp check to keepalive script

--- roles/keepalived/files/check_pihole.sh
+++ roles/keepalived/files/check_pihole.sh
@@ -1,3 +1,25 @@
 #!/bin/bash
+set -e
 
-[ "$(docker inspect -f "{{.State.Health.Status}}" pihole)" = "healthy" ]
+[ "$(docker inspect -f "{{.State.Health.Status}}" pihole)" = "healthy" ] && HEALTHY=0 || HEALTHY=1
+
+PIHOLE_HOME="/home/ohthehugemanatee/pihole"
+
+# if all of these are true, then return 0, else return 1
+if  [ ${HEALTHY} ]; then
+  # If we own the primary IP.
+  if /usr/sbin/ip a |grep -q 10.10.10.40 ; then
+     # Ensure DHCP is enabled.
+     if ! [ -f ${PIHOLE_HOME}/dnsmasq.d/02-pihole-dhcp.conf ]; then
+       /usr/bin/docker exec -d pihole /usr/local/bin/pihole -a enabledhcp "10.10.10.100" "10.10.10.251" "10.10.10.1" "24" "vert"
+     fi
+  else
+    # Ensure DHCP is disabled.
+    if [ -f ${PIHOLE_HOME}/dnsmasq.d/02-pihole-dhcp.conf ]; then
+        /usr/bin/docker exec -d pihole /usr/local/bin/pihole -a disabledhcp
+    fi
+  fi
+  exit $HEALTHY
+else
+  exit $HEALTHY
+fi
